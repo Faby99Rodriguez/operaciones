15e324801df2a1d1e1becfb82e82b221bc2f8f43
Fabiola_Rodríguez <Fabiola_Rodrguez@LAPTOP-6EF7R5V9>
2021-10-18 00:41:35 -0600

    Add project files.

---
+++ operaciones/Division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace operaciones
+{
+    class Division
+    {
+        public static double Divis(double a, double b)
+        {
+            return a / b;
+        }
+
+
+        public static String MostrarDiv(double a, double b)
+        {
+            double res = a / b;
+            return "El resultado de " + a + "/" + b + "=" + res;
+        }
+    }
+}
---
+++ operaciones/Residuo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace operaciones
+{
+    public class Residuo
+    {
+       
+        public static double resid(double a, double b)
+        {
+            return a % b;
+        }
+
+
+        public static String Mostrar(double a,double b)
+        {
+            double res = a % b;
+            return "El residuo  de " + a + "%" + b + "=" + res;
+        }
+    }
+}
